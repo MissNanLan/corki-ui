453bb0c76c8b03bb6c0c6cf9a1047d6c092412a3
xuya227939 <hezhiqianye@gmail.com>
2020-04-06 16:49:14 +0800

    new: Add affix component

--- src/index.js
+++ src/index.js
@@ -19,3 +19,5 @@ export { default as message } from './components/Message/index.js';
 export { default as Select } from './components/Select/index.js';
 
 export { default as Checkbox } from './components/Checkbox/index.js';
+
+export { default as Affix } from './components/Affix/index.js';
